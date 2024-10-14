f5fb0c0b1d12a4c0704d1a6a5a27836a34eb41c0
guilhermecruz378 <guilhermecruz378@gmail.com>
2024-10-14 15:20:39 -0300

    conversao de tipos

--- aula11/conversao.js
+++ aula11/conversao.js
@@ -1,5 +1,13 @@
 /** ----- conversao ------
  * para conversao devemos usar uma função chamada: parse
  * podemos transformar numeros: inteiros ou reais e stings
+ * exemplos: 
+ * const num1 = parseInt('5'); -> para numeros inteiros
+ * const num1 = parseFloat('5'); -> para numeros reais
+ * const num = Number('5'); -> Não faz distinção de int e float, faz a conversão automatico
  * 
  */
+
+const num1 = parseFloat('10.9');
+const num2 = Number('50');
+console.log(num1 + num2);
