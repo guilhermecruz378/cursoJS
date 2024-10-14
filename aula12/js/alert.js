a5f69318e41907f05c753f713d76c8db09f177a7
guilhermecruz378 <guilhermecruz378@gmail.com>
2024-10-14 16:09:24 -0300

    window

---
+++ aula12/js/alert.js
@@ -0,0 +1,10 @@
+alert('Olá, Mundo!');
+alert('Meus primeiros scripts!');
+/**
+ * Alert é uma função que está dentro do objeto window
+ * podemos escrver window.alert('Olá, mundo!) assim
+ * ira funcionar da mesma maneira
+ * -----
+ * coloque no console no chrome para descobrir as funções no window
+ * 
+ */
