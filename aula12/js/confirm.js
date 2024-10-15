3f41083774cf00b264d94061fdfefd76505d1f29
Guilherme Cruz <guilhermecruz378@gmail.com>
2024-10-15 13:42:41 -0300

    alert, confirm,prompt

---
+++ aula12/js/confirm.js
@@ -0,0 +1,3 @@
+confirm('Deseja confirmar?');
+// para capturar uma resposta usamos constantes ou variavel
+// const confirma = confirm('desja realmente confirmar');
---
+++ aula12/js/prompt.js
@@ -0,0 +1,7 @@
+prompt('Digite o seu nome?');
+// podemos fazer calculos no console / pedir numeros
+// let num1 = prompt('digite um numero');
+// e depois fazer a tranformação: 
+// nu1 = Number(num1)
+// ou direto no pedido do numero:
+// let num1 = Number(prompt('Digite um numero))
