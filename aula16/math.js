144b1e89eabd39ac8079b43a727c7691607b2059
Guilherme Cruz <guilhermecruz378@gmail.com>
2024-10-17 16:18:07 -0300

    exercio

--- aula16/math.js
+++ aula16/math.js
@@ -1,1 +1,20 @@
 // math == Funçoes de matematica 
+/*let num1 = 9.89858
+// let num2 = Math.floor(num1) //=>arredonda pr baixo 
+// let num2 = Math.ceil(num1) //=> arredonda pr cima
+// let num2 = Math.round(num1) //=> arredonda para o mais proximo
+
+console.log(num2)
+*/
+
+
+//console.log(Math.max(1,2,3,4,5,6,1500));//acha o maior
+//console.log(Math.min(1,2,3,4,5,6,1500));//acha o menor
+
+/*
+const aleatorio = Math.round(Math.random() * (10 - 5) + 5); // para descobrir um número aleatorio e arredondalo para inteiros
+console.log(aleatorio);
+*/
+
+let num1 = 9;
+console.log(Math.sqrt(num1));
