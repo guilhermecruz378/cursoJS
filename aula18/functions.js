1e7e03811505ca8176d14b60f205536e445e45fd
Guilherme Cruz <guilhermecruz378@gmail.com>
2024-10-18 11:21:12 -0300

    functions

---
+++ aula18/functions.js
@@ -0,0 +1,11 @@
+function saudacao(nome) {
+    //console.log(`Bom dia ${nome}!`);
+    return `Bom dia ${nome}!`;
+}
+
+const variavel = saudacao('Guilherme');
+console.log(variavel);
+
+//saudacao('Douglas');
+//saudacao('Gui');
+//saudacao('Silva');
---
+++ aula18/functionsoma.js
@@ -0,0 +1,9 @@
+function soma(x = 0, y = 0) {
+    const resultado = x + y;
+    return resultado;
+}
+
+console.log();
+
+//console.log(soma(3, 15));
+//console.log(soma(9, 9));
