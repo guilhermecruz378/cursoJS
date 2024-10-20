dd88a8ef218c2e5e42894f42ac7d9d0b620127dd
Guilherme Cruz <guilhermecruz378@gmail.com>
2024-10-20 18:33:44 -0300

    exercicio

--- aula21/js/script.js
+++ aula21/js/script.js
@@ -1,16 +1,27 @@
-function meuEscopo () {
+function meuEscopo() {
     const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultado');
+
+    const pessoas = [nome.value, sobrenome.value, peso.value, altura.value];
     
-    function recebeEventoForm (Evento) {
-        Evento.preventDefault();
+    function recebeEventoForm (evento) {
+        evento.preventDefault();
 
         const nome = form.querySelector('.nome');
         const sobrenome = form.querySelector('.sobrenome');
         const peso = form.querySelector('.peso');
         const altura = form.querySelector('.altura');
 
-        console.log(nome, sobrenome, peso, altura)
-    }
+        pessoas.push({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value
+        });
+        console.log(pessoas);
+
+        resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ${peso.value} ${altura.value}</p>`;
+    };
 
     form.addEventListener('submit', recebeEventoForm);
 }
