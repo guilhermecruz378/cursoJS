f2c6b5c15348dc024a8debf443c6407c6221df29
Guilherme Cruz <guilhermecruz378@gmail.com>
2024-10-21 11:59:18 -0300

    operadores

---
+++ aula22/comparadores.js
@@ -0,0 +1,20 @@
+/*
+Operadores de comparação
+> maior que 
+>= maior que igual a
+< menor que 
+<= menor que igual
+== igualdade (valor) ******
+=== igualdade estrita (valor e tipo)
+!= diferente (valor) ******
+!== diferente estrito (valor e tipo)
+*/
+const a = 15; //Number
+const b = '15'; //string
+const c = 15; // Number
+
+console.log(a == b);
+console.log(a === c);
+console.log(b !== c);
+console.log(b != c);
+console.log(b != a);
---
+++ aula23/logicos.js
@@ -0,0 +1,25 @@
+/*
+Operadores Lógicos
+&& => AND => E === Todas as expressoes precisam ser verdadeiras
+|| => OR => OU === Basta que uma expressão seja verdadeira se ambas forem false ira retornar falso
+! => NOT => NÃO
+
+const expressaoAnd = true && true && true && true // Todas as expressoes precisam ser verdadeiras
+const expressaoOr = false || true || false || false // Basta que uma expressão seja verdadeira 
+
+console.log(expressaoOr);
+
+*/
+/* 
+const login = 'Guilherme344';
+const senha = '3609';
+
+const vaiLogar = login === 'Guilherme344' && senha === '36089';
+console.log(vaiLogar);
+
+*/
+
+console.log(!true);
+console.log(!!true);
+console.log(!false);
+console.log(!!false);
