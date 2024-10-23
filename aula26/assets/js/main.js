cac3b221578270e6f55337f02c4a6417dac406ad
Guilherme Cruz <guilhermecruz378@gmail.com>
2024-10-23 16:09:37 -0300

    exercicio

---
+++ aula26/assets/js/main.js
@@ -0,0 +1,11 @@
+function lerDados() {
+    const peso = document.querySelector('.idpeso');
+    const altura = document.querySelector('.idaltura');
+    const resultado = document.querySelector('.resultado');
+
+    function calculo (a = 0, b = 0.0){
+       
+
+    }
+
+}
