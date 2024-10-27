62c882684d97f5593538ba6bdd68a062d313237f
Guilherme Cruz <guilhermecruz378@gmail.com>
2024-10-27 11:06:52 -0300

    função Date

--- aula27/operadorternario.js
+++ aula27/operadorternario.js
@@ -1,5 +1,6 @@
 // Operador ternário é: ?(interrogação) ou :(Dois pontos).
-// Operação ternária => pode substituir o if else 
+// Operação ternária => pode substituir o if e else 
+// (condição) '?'  para valor verdadeiro e ':' para valor falso;
 
 const pontuacaoUsuario = 1000;
 const niveUsuario = pontuacaoUsuario >= 1000 ? 'usuario VIP' : 'Usúario normal'; // foi colocado uma operação condicional em uma linha e em uma variavel, é igua a: 
@@ -11,4 +12,7 @@ if (pontuacaoUsuario >= 1000) {
 }
 */
 
-console.log(niveUsuario);
+const corUsuario = 'Branco'; // cor que o usuario vai escolher
+const corPadrao = corUsuario || 'Preta'; // se o usuario não escolher a cor sera preta => operação ternária na variavel
+
+console.log(niveUsuario, corPadrao); // vai mostrar o resultado das operações ternárias.
---
+++ aula28/date.js
@@ -0,0 +1,4 @@
+// A funçãao date é uma função construtora => uma função construtora sempre começa com a letra maiuscula
+
+const data = new Date(); // para udar essa função devemos usar a palavra 'new' antes da função. O jS vai criar um novo objeto de data sendo dia, hora, milesimo, etc...
+console.log(data);
