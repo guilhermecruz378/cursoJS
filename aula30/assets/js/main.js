35cc332af8df5cc0ac8143e5cf426392b455e7b1
Guilherme Cruz <guilhermecruz378@gmail.com>
2024-10-29 15:39:02 -0300

    exercicio

---
+++ aula30/assets/js/main.js
@@ -0,0 +1,102 @@
+const container = document.querySelector('.container');
+const data = document.querySelector('#data')
+
+function zeroAesquerda(num) {
+    return num >= 10 ? num : `0${num}`; 
+}
+
+
+function formataData(data) {
+    let diaSemana = data.getDay(); 
+
+    if (diaSemana === 0) {
+        diaSemana = 'Domingo';
+
+    } else if (diaSemana === 1) {
+        diaSemana = 'Segunda-feira';
+
+    } else if (diaSemana === 2) {
+        diaSemana = 'Terça-feira';
+
+    } else if (diaSemana === 3) {
+        diaSemana = 'Quarta-feira';
+        
+    } else if (diaSemana === 4) {
+        diaSemana = 'Quinta-feira';
+        
+    } else if (diaSemana === 5) {
+        diaSemana = 'Sexta-feira';
+        
+    } else if (diaSemana === 6) {
+        diaSemana = 'Sábado';
+        
+    } 
+    const diaNum = zeroAesquerda(data.getDate());
+    let mes = data.getMonth() + 1;
+
+    if (mes === 1) {
+        mes = 'Janeiro';
+
+    } else if (mes === 2) {
+        mes = 'Fevereiro';
+
+    } else if (mes === 3) {
+        mes = 'Março';
+
+    } else if (mes === 4) {
+        mes = 'Abril';
+        
+    } else if (mes === 5) {
+        mes = 'Maio';
+        
+    } else if (mes === 6) {
+        mes = 'Junho';
+        
+    } else if (mes === 7) {
+        mes = 'Julho';
+        
+    } else if (mes === 8) {
+        mes = 'Agosto';
+        
+    } else if (mes === 9) {
+        mes = 'Setembro';
+        
+    } else if (mes === 10) {
+        mes = 'Outubro';
+        
+    } else if (mes === 11) {
+        mes = 'Novembro';
+        
+    } else if (mes === 12) {
+        mes = 'Dezembro';
+        
+    } 
+    const ano = zeroAesquerda(data.getFullYear());
+    const hora = zeroAesquerda(data.getHours());
+    const min = zeroAesquerda(data.getMinutes());
+
+    return `${diaSemana}, ${diaNum} de ${mes} de ${ano} ${hora}:${min}`;
+
+}
+
+const dataNova = new Date();
+let dataAtual = formataData(dataNova);
+//console.log(dataAtual);
+dataAtual.innerHTML = `${data.value}`;
+
+function criaH1 (data) {
+    const h = document.createElement('H1');
+    return h;
+}
+
+function mostrarNaTela (data) {
+
+    const resultado = document.querySelector('.contaniner');
+    resultado.innerHTML = '';
+
+    const h = criaH1(dataAtual);
+    h.innerHTML = data
+    resultado.appendChild(h)
+    
+}
+
