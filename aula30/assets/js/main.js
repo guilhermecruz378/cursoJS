435bf5eb3cc4dae9efc9feaac423c57edc66317c
Guilherme Cruz <guilhermecruz378@gmail.com>
2024-10-29 22:58:57 -0300

    exercicio switch case e Date

--- aula30/assets/js/main.js
+++ aula30/assets/js/main.js
@@ -1,15 +1,33 @@
 const container = document.querySelector('.container');
-const data = document.querySelector('#data')
+const resultado = document.querySelector('#resultado')
 
 function zeroAesquerda(num) {
     return num >= 10 ? num : `0${num}`; 
 }
 
 
+
 function formataData(data) {
     let diaSemana = data.getDay(); 
+    switch (diaSemana){
+        case 0: diaSemana = 'Domingo'
+        break
+        case 1: diaSemana = 'Segunda-feira'
+        break
+        case 2: diaSemana = 'Terça-feira'
+        break
+        case 3: diaSemana = 'Quarta-feira'
+        break
+        case 4: diaSemana = 'Quita-feira'
+        break
+        case 5: diaSemana = 'Sexta-feira'
+        break
+        case 6: diaSemana = 'Sábado'
+        
+    }
 
-    if (diaSemana === 0) {
+   /* Expressao acima no formato de if
+   if (diaSemana === 0) {
         diaSemana = 'Domingo';
 
     } else if (diaSemana === 1) {
@@ -30,10 +48,38 @@ function formataData(data) {
     } else if (diaSemana === 6) {
         diaSemana = 'Sábado';
         
-    } 
+    } */
     const diaNum = data.getDate();
     let mes = data.getMonth() + 1;
 
+    switch (mes) {
+        case 1: mes = 'janeiro'
+        break
+        case 2: mes = 'Fevereiro'
+        break
+        case 3: mes = 'Março'
+        break
+        case 4: mes = 'Abril'
+        break
+        case 5: mes = 'Maio'
+        break
+        case 6: mes = 'Junho'
+        break
+        case 7: mes = 'Julho'
+        break
+        case 8: mes = 'Agosto'
+        break
+        case 9: mes = 'Setembro'
+        break
+        case 10: mes = 'Outubro'
+        break
+        case 11: mes = 'Novembro'
+        break
+        case 12: mes = 'Dezembro'
+        break
+    }
+
+    /* Expressao acima no formato de if
     if (mes === 1) {
         mes = 'Janeiro';
 
@@ -70,7 +116,7 @@ function formataData(data) {
     } else if (mes === 12) {
         mes = 'Dezembro';
         
-    } 
+    } */
     const ano = zeroAesquerda(data.getFullYear());
     const hora = zeroAesquerda(data.getHours());
     const min = zeroAesquerda(data.getMinutes());
@@ -81,4 +127,5 @@ function formataData(data) {
 
 const dataNova = new Date();
 let dataAtual = formataData(dataNova);
-dataAtual.innerHTML = `${data.value}`;
+resultado.innerHTML = '';
+resultado.innerHTML = `<h1> ${dataAtual} </h1>`;
