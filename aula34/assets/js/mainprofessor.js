5a236774752778182ce7ef4e129f72d6f94de9db
guilhermecruz378 <guilhermecruz378@gmail.com>
2024-11-06 14:54:59 -0300

    finalizando o exercicio

--- aula34/assets/js/mainprofessor.js
+++ aula34/assets/js/mainprofessor.js
@@ -1,5 +1,5 @@
 const array = [
-    {tag: 'p', texto: 'frase 1'},
+    {tag: 'p', texto: 'Criando "p"'},
     {tag: 'div', texto: 'frase 2'},
     {tag: 'footer', texto: 'frase 3'},
     {tag: 'section', texto: 'frase 4'}
@@ -11,7 +11,11 @@ const div = document.createElement('div'); // criando uma div
 for (let i = 0; i < array.length; i++) { // criou o laço 
     let {tag, texto} = array[i] // desestruturando o array 
     let criaTag = document.createElement(tag); // a cada laço vai criando cada tag(objeto) dento da chave desestruturada
-    criaTag.innerHTML += texto; // dentro de cada tag vai ser adicionado o objeto dentro da chave texto
+    //criaTag.innerHTML += texto; // dentro de cada tag vai ser adicionado o objeto dentro da chave texto
+    //criaTag.innerText += texto; // Faz a mesma coisa que o innerHTML so que para texto
+
+    let criaTexto = document.createTextNode(texto);
+    criaTag.appendChild(criaTexto)
     div.appendChild(criaTag); // colocando as tags criadas dentro da div 
 }
 
