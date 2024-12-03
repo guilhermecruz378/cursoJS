38c003ec77d4999641637d5cc06b5fe699e7bf4d
Guilherme Cruz <guilhermecruz378@gmail.com>
2024-12-03 19:07:07 -0300

    função

---
+++ aula47/parametrosdafunction.js
@@ -0,0 +1,15 @@
+// arguments é uma palavra-chave especial usada dentro de funções. Ela é como uma "bolsa mágica" que contém todos os valores passados para uma função, mesmo que você não tenha definido nomes para eles nos parâmetros. 
+// Imagine que uma função é como uma máquina que recebe ingredientes. arguments é a forma de acessar todos os ingredientes entregues, independentemente de como você os rotulou.
+
+function argumentos(a = 0, b = 2, c = 20) {
+    //let nuns = 0;
+
+    /*for (let arg of arguments) { // arg esta armazenando os valores que estão dentro de arguments
+        nuns += arg;
+    }*/
+
+    console.log( a,b,c);
+}
+
+argumentos(1,7);
+argumentos(1,undefined, 10); // fazendo dessa forma eu estou pedindo para que o js assuma o valor padrão que já esta na função
