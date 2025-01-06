f6b939351355dce3f5d2b5578380236c04120031
Guilherme Cruz <guilhermecruz378@gmail.com>
2025-01-06 19:59:42 -0300

    filter

--- aula61/arrayfilter3.js
+++ aula61/arrayfilter3.js
@@ -1,5 +1,5 @@
 // como o filter+callback com todos os indicies funcionam
-
+// Filter => sempre retornar um array, com a mesma quantidade de elementos ou menos.
 const numeros = [5,50,80,1,2,3,5,8,7,11,15,22,27];
 
 const numerosFiltrados = numeros.filter(function(valor, indicie, array) { 
---
+++ aula61/arrayfilter4.js
@@ -0,0 +1,21 @@
+// retorne as pessoas que tem o nome com 5 letras ou mais
+
+const pessoas = [
+    {nome: 'Luiz', idade: 62},
+    {nome: 'Maria', idade: 23},
+    {nome: 'Eduardo', idade:55},
+    {nome: 'Leticia', idade: 19},
+    {nome: 'Rosana', idade: 32},
+    {nome: 'Wallace', idade: 47},
+];
+
+const cincoLetras = pessoas.filter(obj => obj.nome.length>= 5);
+console.log(cincoLetras);
+// const maiorQue50 = pessoas.filter(obj => obj.idade > 50);
+// console.log(maiorQue50);
+// const terminaA = pessoas.filter(obj => obj.nome.toLowerCase().endsWith('a'));
+// console.log(terminaA);
+
+const json = JSON.stringify(cincoLetras);
+json.
+console.log(json)
