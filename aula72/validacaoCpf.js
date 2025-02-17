7de6df58532853c206ee88d3d501476c0eff3e43
Guilherme Cruz <guilhermecruz378@gmail.com>
2025-02-17 13:25:07 -0300

    validação de cpf

--- aula72/validacaoCpf.js
+++ aula72/validacaoCpf.js
@@ -11,7 +11,27 @@
 11 - (total % 11 = ? ) => segundo digito
 */
 
+/*
 let cpf = '705.484.450-52';
 let cpfLimpo = cpf.replace(/\D+/g, '');
 cpfArray = Array.from(cpfLimpo);
 console.log(cpfArray.reduce((ac, val) => ac + Number(val), 0));
+*/
+
+function ValidaCPF(cpfEnviado) {
+    Object.defineProperty(this, 'cpfLimpo', {
+        enumerable: true,
+        get: function() {
+            return cpfEnviado.replace(/\D+/g, '');
+        }
+    });
+}
+ValidaCPF.prototype.valida = function () {
+    if(typeof this.cpfLimpo === 'undefined') return false;
+    if(this.cpfLimpo.length !== 11) return false;
+    return true;
+};
+
+const cpf = new ValidaCPF('705.484.450-52')
+//console.log(cpf.cpfLimpo)
+console.log(cpf.valida());
