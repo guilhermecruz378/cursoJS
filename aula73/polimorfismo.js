e991767760560a4caa7d8bd78c3b9701315b8204
Guilherme Cruz <guilhermecruz378@gmail.com>
2025-02-20 21:44:39 -0300

    polimorfismo

--- aula73/polimorfismo.js
+++ aula73/polimorfismo.js
@@ -8,21 +8,57 @@ function Conta(agencia, conta, saldo) {
 
 Conta.prototype.sacar = function(valor) {
     if(this.saldo < valor) {
-        this.verSaldo()
+        console.log(`Saldo insuficiente: Saldo$${this.saldo.toFixed(2)}`)
         return;
     };
     
     this.saldo -= valor
-   
+    this.verSaldo()
 };
 Conta.prototype.depositar = function(valor) {
     this.saldo += valor
     this.verSaldo()
 };
 Conta.prototype.verSaldo = function() {
-    console.log(`ag.:${this.agencia} | conta.:${this.conta} | Saldo$ : ${this.saldo}`)
+    console.log(`ag.:${this.agencia} | conta.:${this.conta} | Saldo$ : ${this.saldo.toFixed(2)}`)
 };
 
-const conta1 = new Conta(141,454,100);
-conta1.verSaldo()
+// const conta1 = new Conta(141,454,100);
+// conta1.sacar(200)
 //console.log(conta1.verSaldo())
+
+function ContaCorrente(agencia, conta, saldo, limite) {
+    Conta.call(this,agencia, conta, saldo);
+    this.limite = limite;
+};
+
+ContaCorrente.prototype = Object.create(Conta.prototype);
+ContaCorrente.prototype.constructor = ContaCorrente;
+ContaCorrente.prototype.sacar = function(valor) {
+    if(valor > (this.saldo + this.limite)) {
+        console.log(`Saldo insuficiente: Saldo$${this.saldo.toFixed(2)}`)
+        return;
+    }
+
+    this.saldo -= valor;
+    this.verSaldo();
+}
+
+function CP(agencia, conta, saldo) {
+    Conta.call(this,agencia, conta, saldo);
+};
+
+CP.prototype = Object.create(Conta.prototype);
+CP.prototype.constructor = CP;
+
+const cc = new ContaCorrente(11, 22, 0.00, 100);
+cc.depositar(10)
+cc.sacar(110);
+cc.sacar(1);
+
+console.log('-=--=-=-=-=-==-=-=-==-=-')
+
+const cp = new CP(12, 33, 0.00);
+cp.depositar(10)
+cp.sacar(110);
+cp.sacar(1);
