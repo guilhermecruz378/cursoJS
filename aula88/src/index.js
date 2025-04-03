9ea64af06c4fc8ef18d369ac31b3409dad93c40f
Guilherme Cruz <guilhermecruz378@gmail.com>
2025-04-03 20:13:04 -0300

    webpack

--- aula88/src/index.js
+++ aula88/src/index.js
@@ -1,1 +1,7 @@
-p
+alert('Olá, mundo');
+let signo = window.prompt('Qual seu signo?'); 
+if(signo.toLowerCase() === 'aries') {
+    alert('Legal sou de aries também!!')
+} else {
+    alert('Que legal!')
+}
