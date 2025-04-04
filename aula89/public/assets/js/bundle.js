616c01ea4454d92704ca93993ab0919efce73308
Guilherme Cruz <guilhermecruz378@gmail.com>
2025-04-04 09:32:20 -0300

    aula89

---
+++ aula89/public/assets/js/bundle.js
@@ -0,0 +1,14 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+alert('Olá, mundo');
+var signo = window.prompt('Qual seu signo?');
+if (signo.toLowerCase() === 'aries') {
+  alert('Legal sou de aries também!!');
+} else {
+  alert('Que legal!');
+}
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
---
+++ aula89/src/index.js
@@ -0,0 +1,7 @@
+alert('Olá, mundo');
+let signo = window.prompt('Qual seu signo?'); 
+if(signo.toLowerCase() === 'aries') {
+    alert('Legal sou de aries também!!')
+} else {
+    alert('Que legal!')
+}
